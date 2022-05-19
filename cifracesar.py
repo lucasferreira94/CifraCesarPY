550dceb27e9acc1aa82acb03797e01327e269c41
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-05-19 08:56:48 -0300

    cifra cesar atualizado

--- cifracesar.py
+++ cifracesar.py
@@ -7,14 +7,29 @@ key = 5
 def encryption():
     indice = 0
     resultado = ''
-    file = open('plaintext.txt', 'a+')
+    file = open('plaintext.txt', 'r')
     for linha in file:
-        for letra in linha:  # ITERANDO SOBRE CADA CARACTERE DO TEXTO
+        for letra in linha:                           # ITERANDO SOBRE CADA CARACTERE DO TEXTO
             if letra in lc:
-                index = lc.find(letra)  # ENCONTRA O INDICE EXATO DO CARACTERE
-                index = (index + key) % 26
+                index = lc.find(letra)                # ENCONTRA O INDICE EXATO DO CARACTERE
+                index = (index + key) % 26            # CÁLCULO PARA REALIZAR A CIFRA DO CARACTERE
+                resultado += lc[index]                # CONCATENA O RESULTADO COM OS ANTERIORES
+                file_2 = open('ciphertext.txt', 'w+')
+                file_2.write(resultado)
+
+def decrypt():
+    indice = 0
+    resultado = ''
+    file = open('ciphertext.txt', 'r')
+    for linha in file:
+        for letra in linha:
+            if letra in lc:
+                index = lc.find(letra)
+                index = (index - key) % 26
                 resultado += lc[index]
-    return resultado
+                file_2 = open('plaintext.txt', 'w+')
+                file_2.write(resultado)
+    return print(file_2.read())
 
 while True:
     mode = str(input('Qual será o modo? [enc] ou [dec]: '))
