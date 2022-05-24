18e9f9414842c3d6f43c2f5fb098615fa6657238
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-05-24 08:42:37 -0300

    Cometarios alterados

--- cifracesar.py
+++ cifracesar.py
@@ -9,9 +9,9 @@ def encryption():
     file = open('plaintext.txt', 'r')
     for linha in file:
         for letra in linha:                           # ITERANDO SOBRE CADA CARACTERE DO TEXTO
-            if letra in lc:
-                index = lc.find(letra)                # ENCONTRA O INDICE EXATO DO CARACTERE
-                index = (index + key) % 26            # CÁLCULO PARA REALIZAR A CIFRA DO CARACTERE
+            if letra in lc:                           # LETRA ESTA PRESENTE NOS CARACTERES MINUSCULOS IMPORTADOS?
+                index = lc.find(letra)                # ENCONTRA O INDICE(POSICAO) EXATO DO CARACTERE
+                index = (index + key) % 26            # CÁLCULO PARA REALIZAR A CIFRA DO CARACTERE (ROTACIONAR)
                 resultado += lc[index]                # CONCATENA O RESULTADO COM OS ANTERIORES
                 file_2 = open('ciphertext.txt', 'w+')
                 file_2.write(resultado)
